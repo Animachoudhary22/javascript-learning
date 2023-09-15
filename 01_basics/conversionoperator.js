ffe9b33a73704d7db5119e4a1f92a47a3ae06317
Animachoudhary22 <74337573+Animachoudhary22@users.noreply.github.com>
2023-09-15 18:09:51 +0000

    conversion

--- 01_basics/conversionoperator.js
+++ 01_basics/conversionoperator.js
@@ -6,8 +6,8 @@ console.log(typeof(score));
 //convert string into number
 let valueInNumber = Number(score)
 let valueInNumber1 = Number(score1)
-console.log(typeof valueInNumber);
-console.log(typeof valueInNumber1);
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber1);
 
 // "33"=>33
 //"33abc" => NaN
@@ -22,5 +22,22 @@ console.log(booleanisLoggedIn);
 //"anima"=>true
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+// ************operations********************************
+let value = 3
+let Negvalue  = -value
+// console.log(Negvalue);
+// console.log(2*2);
+// console.log(2**3)
+let str1 = "hello"
+let str2 = " Anima"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log(2 +"2");
+// console.log(2+2 + "3");
+// console.log("2" + 2 + 3);
+// console.log("hello" + " "+ "aaanima");
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
