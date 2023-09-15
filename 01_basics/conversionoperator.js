4a3dffb8bf8cd0a1508139fae417786aee1f1f60
Animachoudhary22 <74337573+Animachoudhary22@users.noreply.github.com>
2023-09-15 05:36:09 +0000

    conversion of data types

---
+++ 01_basics/conversionoperator.js
@@ -0,0 +1,26 @@
+let score = "33"
+let score1 = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+//convert string into number
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber1);
+
+// "33"=>33
+//"33abc" => NaN
+//true =>1;false=>0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanisLoggedIn);
+// 1 =>true;0=>false
+//" "=>false
+//"anima"=>true
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
